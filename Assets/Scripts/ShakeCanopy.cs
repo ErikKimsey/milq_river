772ff6229064c20f3700a4a8f8672f6d7344fd19
Erik <kimseyerik@gmail.com>
2020-08-19 10:38:31 -0400

    Created ShakeCanopy script

---
+++ Assets/Scripts/ShakeCanopy.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShakeCanopy : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    /**
+    -- Canopy object needs collision detector,
+    -- On collision:
+    ---- Animate canopy object as shaking, then...
+    ---- Play associated particle system,
+    ---- Particles segue into psychedelic scene transition
+    
+    */
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
